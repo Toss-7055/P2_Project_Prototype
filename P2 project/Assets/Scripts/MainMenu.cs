c127933887b17a8c315a410261680c354acaaea0
Toss <56518414+Toss-7055@users.noreply.github.com>
2020-04-20 15:13:10 +0200

    Base

---
+++ P2 project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Used to change scene in unity. See https://www.youtube.com/watch?v=zc8ac_qUXQY for more
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit(); // Quits the application. Not used in our game but a function that is nice to know
+    }
+}
---
+++ P2 project/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio; // Does so you can work with the audio in your project. See https://www.youtube.com/watch?v=YOaYQrN1oYQ for more
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer; // Makes the AudioMixer able to be worked with
+
+    public void SetVolume(float volume) // Function that allows you to set the volume of the audio in the game. This only affects the master volume. 
+    {
+        audioMixer.SetFloat("Volume", volume); 
+    }
+}
